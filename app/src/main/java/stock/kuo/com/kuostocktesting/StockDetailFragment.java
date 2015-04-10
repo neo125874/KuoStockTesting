f085a004f16fd1fda37872279430a135f1e71663
Jim <neo125874@gmail.com>
2015-04-10 15:35:45 +0800

    Initial commit

---
+++ app/src/main/java/stock/kuo/com/kuostocktesting/StockDetailFragment.java
@@ -0,0 +1,62 @@
+package stock.kuo.com.kuostocktesting;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import stock.kuo.com.kuostocktesting.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Stock detail screen.
+ * This fragment is either contained in a {@link StockListActivity}
+ * in two-pane mode (on tablets) or a {@link StockDetailActivity}
+ * on handsets.
+ */
+public class StockDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public StockDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_stock_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.stock_detail)).setText(mItem.content);
+        }
+
+        return rootView;
+    }
+}
